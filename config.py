635ba02c4f3c81c9cf541e5c2eb80d0ef1f6b8d7
Заславский Ярослав Максимович <blackfyz00@gmail.com>
2024-12-08 14:28:19 +0600

    edits

--- config.py
+++
@@ -1,3 +0,0 @@
-token = '7768244289:AAFUw576HxLVrPW-0hnx4Prd0uW2HDlvrb4'
-init_message = 'Здравствуйте! Я помогу Вам записаться на экзамен, редактировать очереди и посмотреть списки.'
-sudo_password = "IMADMIN"
